7ede29d98992fc4368809339024fd7f7a76dd447
Will <wsg.online@gmail.com>
2022-06-22 13:02:40 -0300

    feat: created a basic template for login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,3 +1,28 @@
 export default function Login() {
-  return <div>Login</div>;
+  return (
+    <div className="container vh-100 d-flex justify-content-center align-items-center">
+      <form className="d-flex flex-column justify-content-center">
+        <div className="mb-3">
+          <input
+            type="email"
+            className="form-control"
+            id="exampleInputEmail1"
+            aria-describedby="emailHelp"
+            placeholder="E-mail"
+          />
+        </div>
+        <div className="mb-3">
+          <input
+            type="password"
+            className="form-control"
+            id="exampleInputPassword1"
+            placeholder="Senha"
+          />
+        </div>
+        <button type="submit" className="btn btn-dark">
+          Acessar
+        </button>
+      </form>
+    </div>
+  );
 }
